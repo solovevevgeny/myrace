cfd0b4236c6c009a0a1d55ded9f5e1e24683ebd9
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-01-18 15:40:06 +0300

    events controller + route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\api\EventController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::GET("/events/",[EventController::class,'index'])->name('event.index');
