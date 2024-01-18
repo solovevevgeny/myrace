abf3eef02496f24ce5a9d303f5e1027da14dff6f
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-01-18 13:08:09 +0300

    add fields for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string("second_name");
+            $table->date("birthday");
+            $table->unsignedBigInteger("club_id");
+            $table->string("phone");
+            $table->string("emergency_phone");
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
