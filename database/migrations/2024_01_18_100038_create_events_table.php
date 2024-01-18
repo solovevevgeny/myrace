5abce30800214d7923fc2b9a41463dd0beb14539
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-01-18 13:03:35 +0300

    Event model with migration

---
+++ database/migrations/2024_01_18_100038_create_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->fillable();
+            $table->date("start")->fillable();
+            $table->string("location")->fillable();
+            $table->unsignedBigInteger("type_id")->fillable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
